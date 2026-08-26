9f5503ea5f211ddbd7f546492c86209d42f0d246
Sajid <yeamimhossainsajid@gmail.com>
2026-08-26 14:53:11 +0600

    feat(discovery): add environment profile properties and context tests

---
+++ services/discovery-server/src/test/java/com/codemonk/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codemonk.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
